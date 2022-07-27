53e5e9e8a32f6a2f7a21d7977c352ee9c0bb39e2
DESKTOP-QTCS1OD\Alex <alex.delderfield@gmail.com>
2022-07-27 21:14:02 +0930

    starting with a basic batch script for data collection

---
+++ RunStatify.bat
@@ -0,0 +1,27 @@
+::RunStatify
+::Commands and functions to manage your files and generate data
+::Might replace this with JS if I can, but this will do for now
+
+::Init
+@ECHO "test"
+
+::Generate folder structure
+::  [DayXX] -> 
+::      [raw code]
+::      [optimised code]
+
+::Iterate folder structure, create master data file
+
+
+::basic data record - file size / day
+
+::* read in from webapp 
+
+
+
+::Additional/TODO
+::Process code via command? 
+
+::Setup purely with JS (if thats even practical/possible)
+
+::Extra stat display information (%increase, decrease, code %s etc)
